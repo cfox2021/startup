7fc0be8267f2a3f8806fd157f04f6b36fed7cf6d
cfox2021 <cfox2021@byu.edu>
2025-10-08 19:44:08 -0600

    Updated header

--- src/app.jsx
+++ src/app.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
-import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, NavLink, Route, Routes, useLocation } from 'react-router-dom';
 import Game from './game/game';
 import Leaderboard from './leaderboard/leaderboard';
 
-export default function App() {
-  return(
-    <BrowserRouter>
-        <div>
-            <header>
+function Header(){
+    const location = useLocation();
+    const isOnLeaderboard = location.pathname === '/leaderboard';
+    
+    return(
+        <header>
             <div className="site_name">
                 <h1>Rhythm Monkey</h1>
                 <NavLink to="https://github.com/cfox2021/startup.git" target="_blank">GitHub</NavLink>
@@ -20,7 +21,13 @@ export default function App() {
             <div className="login_leaderboard">
                 <nav>
                 <ul>
-                    <li><NavLink to="leaderboard">Leaderboard</NavLink></li>
+                    <li>
+                        {isOnLeaderboard ? (
+                            <NavLink to="/">Game</NavLink>
+                        ) : (
+                            <NavLink to="/leaderboard">Leaderboard</NavLink>
+                        )}
+                    </li>
                 </ul>
                 </nav>
 
@@ -36,7 +43,15 @@ export default function App() {
             <div className="username">
                 <p id="player-username">Welcome, <span>[username]</span></p>
             </div>
-            </header>
+        </header>
+    )
+}
+
+export default function App() {
+  return(
+    <BrowserRouter>
+        <div>
+            <Header />
 
         <Routes>
         <Route path='/' element={<Game />} exact />
