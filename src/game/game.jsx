303c354d2f9fce3768e7934e93d63eb11b2fc05f
cfox2021 <cfox2021@byu.edu>
2025-10-23 17:51:41 -0600

    Added monkey image and hands to site and formatted them properly.

--- src/game/game.jsx
+++ src/game/game.jsx
@@ -2,27 +2,70 @@ import React from 'react';
 import '../app.css';
 
 export default function Game() {
-  return(
-  <main>
-    <div id="game-outline" style={{ width: "960px", height: "720px", border: "2px solid #baffc9" }}>
-      <img src="game_placeholder.png" alt="Image preview of game" width="700"/>
-    </div>
+  return (
+    <main>
+      <div
+        id="game-outline"
+        style={{
+          width: "960px",
+          height: "720px",
+          border: "2px solid #baffc9",
+          backgroundColor: "black",
+          position: "relative",
+          overflow: "hidden",
+          margin: "auto"
+        }}
+      >
+        <div
+          style={{
+            position: "absolute",
+            bottom: "180px",
+            left: "50%",
+            transform: "translateX(-50%)",
+            pointerEvents: "none"
+          }}
+        >
+          <img src="/monkey.png" alt="Monkey body" width="360" />
+        </div>
 
-    <section id="highscore">
-      <h2>High Score</h2>
-      <p>[Score grabbed from data base when player logs in, then WebSocket updates appear here when player gets new high score]</p>
-    </section>
+        <div
+          style={{
+            position: "absolute",
+            bottom: "180px", 
+            left: "50%",
+            transform: "translateX(-50%)",
+            display: "flex",
+            alignItems: "flex-end",
+            gap: "360px"
+          }}
+        >
+          <img
+            src="/hand_left.png"
+            alt="Left hand"
+            width="260"
+          />
+          <img
+            src="/hand_right.png"
+            alt="Right hand"
+            width="260"
+          />
+        </div>
+      </div>
 
-    <section id="third-party">
+      <section id="highscore">
+        <h2>High Score</h2>
+        <p>[Score grabbed from data base when player logs in, then WebSocket updates appear here when player gets new high score]</p>
+      </section>
+
+      <section id="third-party">
         <h2>Tutorial Video with YouTube Data API</h2>
         <details>
-            <summary>Show tutorial</summary>
-                <div id="video-outline" style={{ width: "640px", height: "360px", border: "1px solid black" }}>
-                    <p>[Tutorial Will be shown here.]</p>
-                </div>
+          <summary>Show tutorial</summary>
+          <div id="video-outline" style={{ width: "640px", height: "360px", border: "1px solid black" }}>
+            <p>[Tutorial Will be shown here.]</p>
+          </div>
         </details>
-    </section>
-
-  </main>
+      </section>
+    </main>
   );
 }
